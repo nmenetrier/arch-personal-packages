fc4737de560c4daf1ede58cab65cbaa6e9649e17
nmenetrier <nicolas.menetrier@gmail.com>
2019-03-18 01:01:45 +0100

    Doc in run_container.sh .gitignore for haxe & hashlink

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 sudo docker-compose up -d
+
+echo "Container should be started with this name: arch-personal-packages_packaging_1"
+echo "Now execute this command: sudo docker exec -it arch-personal-packages_packaging_1 bash"
